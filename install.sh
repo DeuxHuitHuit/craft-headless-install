3a5b8ab62e8f931bd2f8c0aa9e19a8b31a5572a4
Nicolas Brassard <nitriques@users.noreply.github.com>
2021-09-19 21:43:37 -0400

    Add modules files

--- install.sh
+++ install.sh
@@ -78,6 +78,41 @@ return [
 
 EOF
 
+echo "Add modules files"
+cat > config/app.php << EOF
+<?php
+/**
+ * Yii Application Config
+ *
+ * Edit this file at your own risk!
+ *
+ * The array returned by this file will get merged with
+ * vendor/craftcms/cms/src/config/app.php and app.[web|console].php, when
+ * Craft's bootstrap script is defining the configuration for the entire
+ * application.
+ *
+ * You can define custom modules and system components, and even override the
+ * built-in system components.
+ *
+ * If you want to modify the application config for *only* web requests or
+ * *only* console requests, create an app.web.php or app.console.php file in
+ * your config/ folder, alongside this one.
+ */
+
+use craft\helpers\App;
+
+return [
+    'id' => App::env('APP_ID') ?: 'CraftCMS Headless',
+    'modules' => [
+        'agency-auth' => \modules\agencyauth\AgencyAuth::class,
+    ],
+    'bootstrap' => [
+        'agency-auth',
+    ],
+];
+
+EOF
+
 echo "Add headless config for craft"
 cat > config/general.tmp << EOF
 <?php
