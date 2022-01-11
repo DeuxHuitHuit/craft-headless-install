0effdaf9f896870b7e2ee590cb0f1de959b7232f
Nicolas Brassard <nitriques@users.noreply.github.com>
2022-01-11 15:03:20 -0500

    Add project downloader script

---
+++ download-project.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+HOST="dev@hg2.288dev.com"
+PORT="1023"
+PROJECT="$1"
+
+if [[ -z "$PROJECT" ]]; then
+	echo "ERROR! Please specify a project name.";
+	echo "This should be the name of the folder that contains your Craft project";
+	echo "on the remote (dev) server";
+	exit;
+fi
+
+echo "1. Generating project files"
+ssh -p "$PORT" "$HOST" "cd ~/www/$PROJECT/ && rm -rf config/project && ea-php74 ./craft project-config/write"
+
+echo "2. Downloading project files"
+git rm -r ./project
+scp -r -P "$PORT" "$HOST":"~/www/$PROJECT/config/project" "./"
+
+echo "3. Adding new files to git"
+git add "./project"
+
+echo "Done 👍"
+echo "Please commit the result"
+echo ""
